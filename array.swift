33b92d8a2a5b8b3913bb8fac317b2b47b684c9ba
askari01 <farrukh.askari01@gmail.com>
2016-10-23 19:43:47 +0500

    simple arrays

---
+++ array.swift
@@ -0,0 +1,18 @@
+// simple arrays
+
+/*var a = ["Hello"]
+var b = [1, 2, 3]
+//b += [3, 2, 1] // append
+print (b)*/
+
+// Reverse Array
+var n = [1, 2, 3, 4, 5, 6]
+print(n.count)
+//var temp = 0
+//for i in 1...(n.count  % 2) {
+//  temp = n[n.count - 1]
+//  n[n.count - 1] = i
+//  n[i-1] = temp
+//}
+n.reverse()
+print(n)
